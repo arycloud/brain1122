783b3218c825be03ae676cc0a12940f8869808d1
namiMontana <namimontana@gmail.com>
2017-10-22 08:44:58 +0500

    New version of BRT App

---
+++ article/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import Article
+from . import views
+# Register your models here.
+
+
+@admin.register(Article)
+class ArticleAdmin(ImportExportModelAdmin):
+    resource_class = views.ArticleResource
+#    readonly_fields = ('id',)
---
+++ brain/deploy_settings/__init__.py
@@ -0,0 +1,12 @@
+from brain.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '.herokuapp.com',
+    '0.0.0.0'
+]
+
+SECRET_KEY = ')2m&jhc1n^)dlm95!13+s5!qd2c#fpm!kg)gznqd1ii^t96mca'
